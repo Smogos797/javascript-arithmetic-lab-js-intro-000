3a96de5b1df8a22f8f4ed9428164614fa3522c75
Senai Mogos <smogos797@gmail.com>
2019-01-09 21:31:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-function add() {
+var number = 5
 
+function add() {
+  number += 3
 }
 
 function subtract() {
-
+  number -= 2
 }
 
 function multiply() {
-
+  number *= 3
 }
 
 function divide() {
-
+  number /= 5
 }
