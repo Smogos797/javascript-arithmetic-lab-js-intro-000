7ab837a114c8730a5723ac316227fcf7be7a80b4
Senai Mogos <smogos797@gmail.com>
2019-01-09 22:04:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,55 +1,29 @@
 
-var number = 5
 
-number++ // 5... hmmmm
-
-number // 6 -- the number was incremented after it was evaluated
-
-number-- // 6
-
-number // 5
-
---number // 4
-
-++number // 5
-
-number += 3 // 8
-
-number -= 2 // 3
-
-number *= 10 // 50
-
-number /= 5 // 1
 
 var number = 5
 
-function addby5() {
-  number + 5
-
+function add5() {
+  number += 5
 }
 
-function subtractby2() {
-  number - 2
-
+function subtract2() {
+  number -= 2
 }
 
 function multiplyby2() {
-  number * 2
-
+  number *= 2
 }
 
 function divideby5() {
-  number / 5
-
+  number /= 5
 }
 
-number = 10
-
-addby5()
+add5()
 
 console.log(number)
 
-subtractby2()
+subtract2()
 
 console.log(number)
 
@@ -61,4 +35,4 @@ divideby5()
 
 console.log(number)
 
-parseFloat('80.123999') // 80.123999
+
