31f8b7c67b050f72090fbcb07e8aa4326d2cb1ed
Senai Mogos <smogos797@gmail.com>
2019-01-09 21:41:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,28 +1,14 @@
-var number = 10
+1 + 80 // 81
+60 - 40 // 20
+2 * 3.4 // 6.8 (there's that floating-point arithmetic again...)
+5.0 / 2.5 // 2
 
-function add5() {
-  number += 5
-}
-
-function divideBy3() {
-  number /= 3
-}
-
-divideBy3()
-
-console.log(number) // 3.333333333335
-
-add5()
-
-console.log(number) // 8.333333333335
-
-// reset number
-number = 10
-
-add5()
-
-console.log(number) // 15
-
-divideBy3()
-
-console.log(number) // 5
+var number = 5
+ 
+number++ // 5... hmmmm
+ 
+number // 6 -- the number was incremented after it was evaluated
+ 
+number-- // 6
+ 
+number // 5
