631d95de523c92b2831ee4ab4ace9b1e2d03acb6
Senai Mogos <smogos797@gmail.com>
2019-01-09 21:51:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,22 +2,22 @@
 var number = 5
 
 function addby5() {
-  number += 5
+  number + 5
 
 }
 
 function subtractby2() {
-  number -= 2
+  number - 2
 
 }
 
 function multiplyby2() {
-  number *= 2
+  number * 2
 
 }
 
 function divideby5() {
-  number /= 5
+  number / 5
 
 }
 
