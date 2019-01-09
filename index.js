dc2f26f9eceb88aab715f7250df80187f201f26e
Senai Mogos <smogos797@gmail.com>
2019-01-09 21:38:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
-var number = 5
-
-function add() {
-  number += 3
+var number = 10
+ 
+function add5() {
+  number += 5
 }
-
-function subtract() {
-  number -= 2
+ 
+function divideBy3() {
+  number /= 3
 }
-
-function multiply() {
-  number *= 3
-}
-
-function divide() {
-  number /= 5
-}
-
-add()
-
-console.log(number)
-
-subtract()
-
-console.log(number)
-
-multiply()
-
-console.log(number)
-
-divide()
-
-console.log(number)
+ 
+divideBy3()
+ 
+console.log(number) // 3.333333333335
+ 
+add5()
+ 
+console.log(number) // 8.333333333335
+ 
+// reset number
+number = 10
+ 
+add5()
+ 
+console.log(number) // 15
+ 
+divideBy3()
+ 
+console.log(number) // 5
